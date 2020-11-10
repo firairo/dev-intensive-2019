db5c4eb138a887ef8af52ee74b962fd76492c8cc
Lex Shi <firairo@gmail.com>
2020-11-10 21:06:30 +0300

    Hometask_2 start

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName:String?):Pair<String?, String?>{
+        //TODO FIX ME!!
+        val parts:List<String>? = fullName?.split(" ")
+
+        val firstName =parts?.getOrNull(0)
+        val lastName =parts?.getOrNull(1)
+        //return Pair(firstName, lastName)
+        return firstName to lastName
+    }
+
+    fun transliterations(payload: String, divider:String = " "): String {
+        TODO ("not implemented") // To change bode of created fuctions use File | Settings| File Templates
+
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        TODO ("not implemented") // To change bode of created fuctions use File | Settings| File Templates
+    }
+}
