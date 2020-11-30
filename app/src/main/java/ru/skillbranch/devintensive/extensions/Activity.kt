8fb9d71affb6405ee1f94e17d18f6a4c83ffd99c
Lex Shi <firairo@gmail.com>
2020-11-30 17:43:18 +0300

    Hometask_3 maybe

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,18 +1,31 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
-import android.content.Context
-import android.view.View
-import android.view.WindowManager
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.graphics.Rect
 import android.view.inputmethod.InputMethodManager
 
-fun Activity.hideKeyboard() {
-    val view = this.currentFocus
-    if (view != null) {
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+fun Activity.isKeyboardOpen(): Boolean {
+    val r = Rect()
+    val rootView = window.decorView
+    rootView.getWindowVisibleDisplayFrame(r)
+    val screenHeight = rootView.height
+    var heightDiff = screenHeight - (r.bottom - r.top)
+    val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+    if (resourceId > 0) {
+        heightDiff -= resources.getDimensionPixelSize(resourceId)
     }
-    // else {
-    window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
-    // }
+    if (heightDiff > 100) {
+        return true
+    }
+    return false
+}
+
+fun Activity.isKeyboardClosed() = !isKeyboardOpen()
+
+fun Activity.hideKeyboard() {
+    if (isKeyboardClosed()) return
+    val view = currentFocus ?: return
+    val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
